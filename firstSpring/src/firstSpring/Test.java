02a26685c799e30625667b12b8a7a409106f50b9
cthippan <cthippan@iu.edu>
2017-06-28 15:30:16 -0700

    adding project

---
+++ firstSpring/src/firstSpring/Test.java
@@ -0,0 +1,19 @@
+package firstSpring;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+public class Test {  
+public static void main(String[] args) {  
+//    Resource resource=new ClassPathResource("application.xml");  
+//    BeanFactory factory=new XmlBeanFactory(resource);  
+	ApplicationContext context =   
+		    new ClassPathXmlApplicationContext("application.xml");  
+    Student student=(Student)context.getBean("studentbean");  
+    student.displayInfo();  
+}  
+}  
